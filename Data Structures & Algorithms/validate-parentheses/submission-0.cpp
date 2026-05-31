6cec3dee6d1e0bd04b3fa59f31649c295465231e
chanhisme <chanhle1907@gmail.com>
2026-05-31 11:14:00 +0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack <char> st;
+        for(int i = 0; i < n; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                char ch = st.top();
+                st.pop();
+
+                if(ch != '{' && s[i] == '}') return false;
+                else if(s[i] == ')' && ch != '(') return false;
+                else if (s[i] == ']' && ch != '[') return false;
+                
+            }
+        }
+        return st.empty();
+
+
+
+
+
+
+
+
+
+    }
+};
